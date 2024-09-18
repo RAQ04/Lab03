84560ae2ef7ea827f01f211eb19f32e6404eca04
RAQ04 <Raghdahaq.04@gmail.com>
2024-09-18 20:53:46 +0300

    intial commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        List<String> studentsList= new ArrayList<>();
+        studentsList.add("Danh");
+        studentsList.add("Raghdah");
+
+        System.out.println(studentsList);
+        
+        
     }
 }
